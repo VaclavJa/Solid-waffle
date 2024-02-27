483d320a1adc27e35327b36cfc6bc9d2dd33fd4b
VaclavJa <Vaclav>
2024-02-27 07:53:18 +0100

    Testovaci

--- Python_zaklady/Testovaci.py
+++ Python_zaklady/Testovaci.py
@@ -1,3 +1,14 @@
 print("Tohle je test")
+
+
 jmeno = input("Jaké je tvoje jméno?")
 print(jmeno)
+print(len(jmeno))
+
+Nahodne_cislo = int(input("Zadej nějaké číslo"))
+
+if Nahodne_cislo == str(Nahodne_cislo):
+    print("napiš příště číslo")
+else:  
+    Nahodne_cislo *= 1.5
+    print (Nahodne_cislo)
